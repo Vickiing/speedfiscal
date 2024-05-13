7a402fbe1477365d6a38e71ca43ec46f2fbc95b9
Vickiing <victor.lima.silva@hotmail.com>
2024-05-13 11:49:56 -0300

    tests

--- teste/txt.py
+++ teste/txt.py
@@ -7,7 +7,12 @@ arquivoTXT = r'C:\Users\vlsilva\Documents\PYTHON PROJETOS\fiscal\speedfiscal\tes
 class Txt:
     def __init__(self, bloco=None):
         self.bloco = bloco
-    
+
+
+    def set_bloco(self, bloco):
+        self.bloco =  bloco
+
+
     def ler_txt(self,bloco):
         with open(arquivoTXT, 'r') as txt:
             for linha in txt:
@@ -16,25 +21,25 @@ class Txt:
                     self.bloco = valores[1]
                     linha_inteira = valores
                     #print(f'BLOCO: {self.bloco} - LINHA COMPLETA: {linha_inteira}')
-            
+
 
     def contar_linhas_bloco(self, bloco):
     #precisa verificar se o resultado dessa função é consistente com o erro que o sped informo.
         self.bloco = bloco
+        self.set_bloco(bloco)
         with open(arquivoTXT, 'r') as txt:
             for linha in txt:
                 valores = linha.strip().split('|')
                 if valores[1] == bloco:
                     valores[1] = txt.readlines()
-                    print(len(valores[1]))
-
+                    print(len(valores))
 
 
     def __str__(self) -> str:
         return f'{self.bloco}'
     
 
-    
-efd = Txt()
-efd.contar_linhas_bloco('C110')
 
+efd = Txt()
+efd.contar_linhas_bloco('0000')
+print(efd.bloco)
---
+++ verificaST.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+
+
+sped = r'C:\Users\vlsilva\Documents\PYTHON PROJETOS\python_fiscal\speedfiscal\SPED\EFD_1_46_010524_132948.TXT'
+
+dados = {
+    'Loja': [],
+    'Valor_ICMS': [],
+    'Valor_FECP': []
+}
+
+def func_verificaST(arquivo_sped):
+    
+    loja = None
+    icms = None
+    fecp = None
+
+    with open(arquivo_sped, 'r') as sped:
+        for linha in sped:
+            valores = linha.strip().split('|')
+
+            if valores[1] == '0000':
+                loja = valores[6]
+
+            if valores[1] == 'E250' and valores[2] == '001' and valores[4] == '31052024' and valores[5] == '0230':
+                icms = valores[3]
+
+            if valores[1] == 'E250' and valores[2] == '006' and valores[4] == '31052024' and valores[5] == '7501':
+                fecp = valores[3]
+
+    dados['Loja'].append(loja)
+    dados['Valor_ICMS'].append(icms)
+    dados['Valor_FECP'].append(fecp)
+
+
+caminho_base = r'C:\Users\vlsilva\Documents\PYTHON PROJETOS\python_fiscal\speedfiscal\SPED'
+arquivo = os.listdir(caminho_base)
+caminho_completo = [os.path.join(caminho_base, arquivo) for arquivo in arquivo]
+
+
+def criar_df():
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(func_verificaST, caminho_completo))
+
+    df = pd.DataFrame(dados)
+    return df
+dataframe = criar_df()
+dataframe.fillna(0, inplace=True)
+print(dataframe.sort_values(by='Loja'))
